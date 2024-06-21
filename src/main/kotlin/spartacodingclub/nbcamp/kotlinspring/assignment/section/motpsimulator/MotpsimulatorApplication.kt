475b5ba9570f62acf1a75e5e90e6ec298046769b
turbstructor <ycw0201@naver.com>
2024-06-21 19:08:15 +0900

    :tada: Initial commit

---
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/assignment/section/motpsimulator/MotpsimulatorApplication.kt
@@ -0,0 +1,11 @@
+package spartacodingclub.nbcamp.kotlinspring.assignment.section.motpsimulator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MotpsimulatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<MotpsimulatorApplication>(*args)
+}
---
+++ src/test/kotlin/spartacodingclub/nbcamp/kotlinspring/assignment/section/motpsimulator/MotpsimulatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package spartacodingclub.nbcamp.kotlinspring.assignment.section.motpsimulator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MotpsimulatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
