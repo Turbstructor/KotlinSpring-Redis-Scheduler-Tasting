599681c500b4c086d857c8ee376b17e7fd05d3e0
turbstructor <ycw0201@naver.com>
2024-06-21 20:25:09 +0900

    ✨ Enable Redict

---
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/assignment/section/motpsimulator/infra/redict/config/LettuceConfig.kt
@@ -0,0 +1,35 @@
+package spartacodingclub.nbcamp.kotlinspring.assignment.section.motpsimulator.infra.redict.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+@EnableRedisRepositories
+class LettuceConfig (
+
+    private val redisProperties: RedisProperties
+) {
+
+    @Bean
+    fun redictConnectionFactory(): LettuceConnectionFactory =
+        LettuceConnectionFactory(redisProperties.host, redisProperties.port)
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, String> =
+        RedisTemplate<String, String>().apply {
+            this.connectionFactory = redisConnectionFactory
+
+            this.keySerializer = StringRedisSerializer()
+            this.valueSerializer = StringRedisSerializer()
+
+            this.hashKeySerializer = StringRedisSerializer()
+            this.hashValueSerializer = StringRedisSerializer()
+        }
+}
